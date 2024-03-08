0ea7fbf5a0eaf3c3c49c262e252eba9305ab8188
kotatsu-tf <tatsuya.tf12@gmail.com>
2024-03-08 10:40:18 +0000

    検索欄の表示（形式上） 実際にデータベースから検索ができるわけではないがwebページに形だけ実装

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    name = 'Hello, My Flask!!'
-    return name
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=80, host='0.0.0.0')
