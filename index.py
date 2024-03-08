5c9fe218b369641ec1a32d7d61aa21329e8b0b51
kotatsu-tf <tatsuya.tf12@gmail.com>
2024-03-08 10:03:32 +0000

    Flaskアプリの基本設定とルートルートの追加
    このコミットでは、シンプルなFlaskアプリケーションのセットアップを導入し、ルートURL（'/'）にアクセスした際に挨拶メッセージを返すように設定しています。アプリはデバッグモードで80番ポートで実行され、開発中の変更に対して即時のフィードバックが得られるようになっています。

--- index.py
+++ index.py
@@ -1,1 +1,10 @@
-print('hello, world')
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = 'Hello, My Flask!!'
+    return name
+
+if __name__ == '__main__':
+    app.run(debug=True, port=80)
